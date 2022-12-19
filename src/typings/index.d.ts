fccd3694353f42f9efa193691c419e2f58c2426e
Daniel Kesler <danielckesler@gmail.com>
2022-12-19 10:23:37 -0700

    Fixed devchain script typings, added new tsconfig, added typings for ganache

--- src/typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-// The following files are nested in @Celo dependency but not found by the typescript compiler
-declare module '@celo/flake-tracker/src/jest/setup.global.js'
-declare module '@celo/flake-tracker/src/jest/teardown.global.js'
